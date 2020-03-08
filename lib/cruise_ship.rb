aaad2b4c6cedeb2e6c1c983babc7505d8dc20e76
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-08 04:17:56 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -16,6 +16,11 @@ end
 winner
 end
 def happy_birthday(birthday_kids)
+  birthday_kids = {
+  "Timmy" => 9,
+  "Sarah" => 6,
+  "Amanda" => 27
+}
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
